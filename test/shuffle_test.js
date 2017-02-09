e98064d66a25d01b5f430606c006b3a35eec613e
Diana Vashti <diana.vashti@gmail.com>
2017-02-09 09:30:00 -0800

    stuff

---
+++ test/shuffle_test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import shuffleFactorial from '../src/shuffleFactorial'
+
+describe.only('shuffleFactorial', function(){
+
+  it('should be a function', function(){
+    expect(shuffleFactorial).to.be.a('function')
+  })
+
+  it.only('should do weird stuff', function(){
+    expect(shuffleFactorial('dawg').length).to.deep.equal(24)
+  })
+
+  it('should do even weirder stuff', function(){
+    expect(shuffleFactorial('lose').length).to.deep.equal(60)
+  })
+
+
+})
